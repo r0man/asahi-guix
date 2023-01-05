94efb6035bc22a08e91d01d53fe57036f57f5b5d
r0man <roman@burningswell.com>
2023-01-05 19:18:38 +0000

    Only depend on asahi-guix in installer

--- asahi/installer.scm
+++ asahi/installer.scm
@@ -112,7 +112,6 @@
 
     ;; Add some extra packages useful for the installation process
     (packages
-     (append (list git curl)
-             (operating-system-packages installation-os)))))
+     (append (list asahi-guix) (operating-system-packages installation-os)))))
 
 installation-os-nonfree
