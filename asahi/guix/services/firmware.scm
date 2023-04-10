1d103cba0bc111b0793578a28bfe3c851fc7aa24
Roman Scherer <roman@burningswell.com>
2023-04-10 14:47:11 +0200

    Call blkid

--- asahi/guix/services/firmware.scm
+++ asahi/guix/services/firmware.scm
@@ -1,6 +1,7 @@
 (define-module (asahi guix services firmware)
   #:use-module (asahi guix build firmware)
   #:use-module (asahi guix packages guile-xyz)
+  #:use-module (gnu packages linux)
   #:use-module (gnu services base)
   #:use-module (gnu services)
   #:use-module (guix gexp)
@@ -13,11 +14,13 @@
   (with-extensions (list guile-asahi-guix)
     (with-imported-modules (source-module-closure
                             '((asahi guix build firmware)
+                              (guix build utils)
                               ;; TODO: no code for module (guix cpio)
                               (guix cpio)))
       #~(begin
           (use-modules (asahi guix build firmware))
           (display ":: Asahi Guix: Starting Asahi firmware service ...\n")
+          (invoke "blkid")
           (setup-firmware)
           (display ":: Asahi Guix: Asahi firmware service started.\n")
           #t))))
@@ -26,6 +29,10 @@
   (service-type
    (name 'asahi-firmware)
    (extensions
-    (list (service-extension activation-service-type activate-firmware)))
+    (list (service-extension activation-service-type
+                             activate-firmware)
+          (service-extension profile-service-type
+                             (lambda (config)
+                               (list util-linux)))))
    (description "Service that loads the Apple Silicon firmware at boot time.")
    (default-value #f)))
