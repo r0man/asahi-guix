9ffdc01d66a6ef764216fe3c91debd41fbc9a943
Roman Scherer <roman@burningswell.com>
2023-04-15 16:00:00 +0200

    Add installer image module

--- asahi/guix/images/installer.scm
+++ asahi/guix/images/installer.scm
@@ -1,17 +1,17 @@
 (define-module (asahi guix images installer)
   #:use-module (asahi guix system base)
-  #:use-module (gnu bootloader)
   #:use-module (gnu bootloader u-boot)
+  #:use-module (gnu bootloader)
   #:use-module (gnu image)
-  #:use-module (gnu packages linux)
   #:use-module (gnu packages certs)
-  #:use-module (guix platforms arm)
-  #:use-module (gnu services)
+  #:use-module (gnu packages linux)
   #:use-module (gnu services base)
   #:use-module (gnu services networking)
-  #:use-module (gnu system)
+  #:use-module (gnu services)
   #:use-module (gnu system file-systems)
   #:use-module (gnu system image)
+  #:use-module (gnu system)
+  #:use-module (guix platforms arm)
   #:use-module (srfi srfi-26)
   #:export (asahi-installer-image-type
             asahi-installer-os
@@ -21,18 +21,23 @@
   (operating-system
     (inherit (asahi-operating-system))
     (file-systems
-     (cons (file-system
-             (device (file-system-label "my-root"))
-             (mount-point "/")
-             (type "ext4"))
-           %base-file-systems))))
+     (cons* (file-system
+              (device (file-system-label "GNU-ESP"))
+              (mount-point "/boot/efi")
+              (needed-for-boot? #t)
+              (type "vfat"))
+            (file-system
+              (device (file-system-label root-label))
+              (mount-point "/")
+              (type "ext4"))
+            %base-file-systems))))
 
 (define asahi-installer-image-type
   (image-type
    (name 'asahi-installer-raw)
    (constructor (lambda (os)
                   (image
-                   (inherit (raw-with-offset-disk-image))
+                   (inherit efi-disk-image)
                    (operating-system os)
                    (platform aarch64-linux))))))
 
