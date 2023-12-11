fb549bdca9fc6629e0a592d572d839d85b4e8c60
Roman Scherer <roman@burningswell.com>
2023-12-11 13:51:35 -0300

    Add asahi-guix server machine

--- src/asahi/guix/machine/server.scm
+++ src/asahi/guix/machine/server.scm
@@ -1,8 +1,8 @@
 (define-module (asahi guix machine server)
-  #:use-module (gnu)
   #:use-module (asahi guix system server)
-  #:use-module (gnu machine)
   #:use-module (gnu machine ssh)
+  #:use-module (gnu machine)
+  #:use-module (gnu)
   #:export (asahi-guix-server-machine))
 
 (define asahi-guix-server-machine
@@ -10,10 +10,10 @@
    (operating-system asahi-guix-server-system)
    (environment managed-host-environment-type)
    (configuration (machine-ssh-configuration
-                   (host-name "asahi-guix")
+                   (host-name "ci.asahi-guix.org")
                    (system "aarch64-linux")
                    (user "root")
-                   (identity "~/.ssh/id_rsa")
-                   (port 2222)))))
+                   (identity "/home/roman/.ssh/id_rsa")
+                   (port 22)))))
 
-asahi-guix-server-machine
+(list asahi-guix-server-machine)
